b89de7dac69e0a8df603ca979dad7bd619aca1bd
Amr Elzahar <amr92621@gmail.com>
2023-04-06 22:33:47 +0200

    added deployment files

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 mongoose
-  .connect("mongodb://localhost:27017/products")
+  .connect(`mongodb://${process.env.MONGODB_HOST}:27017/products`)
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
